d4ca25ec3ca5dfac88f4772a023b2df1d7fdd30e
EliotWork <bereznev68@mail.ru>
2022-10-03 00:11:51 +0500

    update

--- app/src/main/java/com/sumin/shoppinglist/presentation/activity/fistActivity/MainActivity.kt
+++ app/src/main/java/com/sumin/shoppinglist/presentation/activity/fistActivity/MainActivity.kt
@@ -1,31 +1,37 @@
-package com.sumin.shoppinglist.presentation
+package com.sumin.shoppinglist.presentation.activity.fistActivity
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
-import com.sumin.shoppinglist.R
+import com.sumin.shoppinglist.databinding.ActivityMainBinding
+import com.sumin.shoppinglist.presentation.activity.secondActivity.ShopItemActivity
 import com.sumin.shoppinglist.presentation.rcView.ShopListAdapter
 import com.sumin.shoppinglist.presentation.viewmodel.MainViewModel
 
 class MainActivity : AppCompatActivity() {
     private lateinit var viewModel: MainViewModel
     private lateinit var shopListAdapter: ShopListAdapter
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         setupRecyclerView()
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
         viewModel.shopList.observe(this) {
             shopListAdapter.submitList(it)
         }
+        binding.buttonAddShopItem.setOnClickListener {
+           val intent =ShopItemActivity.newIntentAddItem(this)
+            startActivity(intent)
+        }
     }
 
-    private fun setupRecyclerView() {
-        val rvShopList = findViewById<RecyclerView>(R.id.rv_shop_list)
+    private fun setupRecyclerView() = with(binding) {
         with(rvShopList) {
             shopListAdapter = ShopListAdapter()
             adapter = shopListAdapter
@@ -68,7 +74,8 @@ class MainActivity : AppCompatActivity() {
 
     private fun setupClickListener() {
         shopListAdapter.onShopItemClickListener = {
-            Log.d("MainActivity", it.toString())
+            val intent =ShopItemActivity.newIntentEditItem(this,it.id)
+            startActivity(intent)
         }
     }
 
@@ -77,4 +84,6 @@ class MainActivity : AppCompatActivity() {
             viewModel.changeEnableState(it)
         }
     }
+
+
 }
---
+++ app/src/main/java/com/sumin/shoppinglist/presentation/activity/secondActivity/ShopItemActivity.kt
@@ -0,0 +1,147 @@
+package com.sumin.shoppinglist.presentation.activity.secondActivity
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import com.sumin.shoppinglist.R
+import com.sumin.shoppinglist.databinding.ActivityShopItemBinding
+import com.sumin.shoppinglist.domain.ShopItem
+import com.sumin.shoppinglist.presentation.viewmodel.ShopItemViewModel
+
+class ShopItemActivity : AppCompatActivity() {
+    private lateinit var viewModel: ShopItemViewModel
+    private lateinit var binding: ActivityShopItemBinding
+    private var screenMode = MODE_UNKNOW
+    private var shopItemId = ShopItem.UNDEFINED_ID
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityShopItemBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        parseIntent()
+        viewModel = ViewModelProvider(this)[ShopItemViewModel::class.java]
+        addTextChangedListener()
+        launchMode()
+        ifErrorNameAndCount()
+    }
+
+    private fun addTextChangedListener() = with(binding) {
+        etName.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                viewModel.resetErrorInputName()
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+
+            }
+
+        })
+        etCount.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                viewModel.resetErrorInpuCount()
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+
+            }
+
+        })
+    }
+
+    private fun launchMode() {
+        when (screenMode) {
+            MODE_EDIT -> {
+                launchEditMode()
+            }
+            MODE_ADD -> {
+                launchAddMode()
+            }
+        }
+    }
+
+    private fun ifErrorNameAndCount() = with(binding) {
+        viewModel.errorinputCount.observe(this@ShopItemActivity) {
+            val message = if (it) {
+                getString(R.string.error_input_count)
+            } else {
+                null
+            }
+            tilCount.error = message
+        }
+
+        viewModel.errorinputCount.observe(this@ShopItemActivity) {
+            val message = if (it) {
+                getString(R.string.error_input_name)
+            } else {
+                null
+            }
+            tilName.error = message
+        }
+        viewModel.shouldCloseScreen.observe(this@ShopItemActivity) {
+            finish()
+        }
+    }
+
+    private fun launchEditMode() = with(binding) {
+        viewModel.getShopItem(shopItemId)
+        viewModel.shopItem.observe(this@ShopItemActivity) {
+            etName.setText(it.name)
+            etCount.setText(it.count.toString())
+        }
+        saveButton.setOnClickListener {
+            viewModel.editShopItem(etName.text?.toString(), etCount.text?.toString())
+        }
+    }
+
+    private fun launchAddMode() = with(binding) {
+        saveButton.setOnClickListener {
+            viewModel.addShopItem(etName.text?.toString(), etCount.text?.toString())
+        }
+    }
+
+    private fun parseIntent() {
+        if (!intent.hasExtra(EXTRA_SCREEN_MODE)) {
+            throw RuntimeException("Param screen mode is  absent")
+        }
+        val mode = intent.getStringExtra(EXTRA_SCREEN_MODE)
+        if (mode != MODE_EDIT && mode != MODE_ADD) {
+            throw RuntimeException("Unknown screen mode  $mode")
+        }
+        screenMode = mode
+        if (screenMode == MODE_EDIT) {
+            if (!intent.hasExtra(EXTRA_SHOP_ITEM_ID)) {
+                throw RuntimeException("Param shop item id  is  absent")
+            }
+            shopItemId = intent.getIntExtra(EXTRA_SHOP_ITEM_ID, ShopItem.UNDEFINED_ID)
+        }
+    }
+
+    companion object {
+        private const val EXTRA_SCREEN_MODE = "extra_mode"
+        private const val EXTRA_SHOP_ITEM_ID = "extra_shop_item_id"
+        private const val MODE_EDIT = "mode_edit"
+        private const val MODE_ADD = "mode_add"
+        private const val MODE_UNKNOW = ""
+        fun newIntentAddItem(context: Context): Intent {
+            val intent = Intent(context, ShopItemActivity::class.java)
+            intent.putExtra(EXTRA_SCREEN_MODE, MODE_ADD)
+            return intent
+        }
+
+        fun newIntentEditItem(context: Context, shopitemId: Int): Intent {
+            val intent = Intent(context, ShopItemActivity::class.java)
+            intent.putExtra(EXTRA_SCREEN_MODE, MODE_EDIT)
+            intent.putExtra(EXTRA_SHOP_ITEM_ID, shopitemId)
+            return intent
+        }
+    }
+}
