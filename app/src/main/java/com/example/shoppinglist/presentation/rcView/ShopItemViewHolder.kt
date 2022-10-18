8f4d2d1e0c00317f31c433681df1325aa140212b
EliotWork <bereznev68@mail.ru>
2022-10-19 01:38:51 +0500

    update

---
+++ app/src/main/java/com/example/shoppinglist/presentation/rcView/ShopItemViewHolder.kt
@@ -0,0 +1,8 @@
+package com.example.shoppinglist.presentation.rcView
+
+import androidx.databinding.ViewDataBinding
+import androidx.recyclerview.widget.RecyclerView
+
+class ShopItemViewHolder(
+    val binding: ViewDataBinding
+    ) : RecyclerView.ViewHolder(binding.root)
